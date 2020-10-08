5e54084421198466557d65a8fba0892e543989bf
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-08 15:49:50 +0200

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors())
 
 const greet = express.Router();
 greet.get('/', (req, res) => {
@@ -19,7 +21,7 @@ app.post('/subscribe', function(req, res) {
 });
 
 const port = process.env.PORT || 80;
- 
+
 app.listen(port, err => {
  if(err) throw err;
  console.log("%c Server running", "color: green");
