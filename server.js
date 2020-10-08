a89ca6546b7e18eefd886c6c927306db3775cc47
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-08 15:43:52 +0200

    make it better

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 const greet = express.Router();
 greet.get('/', (req, res) => {
-  console.log(req.baseUrl);
-  res.send('Hello World');
+ console.log(req.baseUrl);
+ res.send('Hello World');
 })
-
-const subscribe = express.Router();
-subscribe.get('/', (req, res) => {
-  console.log(req.baseUrl);
-  res.send('Hello subscribe');
-})
-
-
-
-
-
 app.use('/greet', greet);
 
-app.use('/subscribe', subscribe);
+app.post('/subscribe', function(req, res) {
+ console.log(req.body);
 
+ res.send(req.body);
+});
 
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const port = process.env.PORT || 80;
+ 
+app.listen(port, err => {
+ if(err) throw err;
+ console.log("%c Server running", "color: green");
+});
