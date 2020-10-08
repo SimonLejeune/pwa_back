1ddb6440af60523fbc4680e9e27ee78dc4ad9103
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-08 17:10:25 +0200

    make it better

--- server.js
+++ server.js
@@ -17,12 +17,15 @@ webpush.setVapidDetails(
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(cors());
+app.use(cors({
+ origin: "https://ngseo.netlify.app"
+}));
+/*app.use(cors());
 app.use(function(req, res, next) {
  res.header("Access-Control-Allow-Origin", "https://ngseo.netlify.app"); // update to match the domain you will make the request from
  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  next();
-});
+});*/
 
 const greet = express.Router();
 greet.get('/', (req, res) => {
