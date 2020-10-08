a90a6a1fb67bc646696b399531408489e1174b47
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-08 17:00:02 +0200

    make it better

--- server.js
+++ server.js
@@ -17,12 +17,7 @@ webpush.setVapidDetails(
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(cors())
-app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "https://serverpwa.herokuapp.com/"); // update to match the domain you will make the request from
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
-});
+app.use(cors({    origin: 'https://ngseo.netlify.app/'}));
 
 const greet = express.Router();
 greet.get('/', (req, res) => {
