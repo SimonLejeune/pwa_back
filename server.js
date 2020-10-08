fa1b793bfec10a28ae415d3145e8d9f51790fa9a
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-08 16:10:25 +0200

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 var cors = require('cors')
 const app = express();
+const webpush = require('web-push');
+
+const vapidKeys = {
+ "publicKey":"BCNVbiY2W4wbKEIEoA7ChkmtKWUB5M4NQ-dN4M1acfFJSPEB2gJpNYbb0AnXT2--pcrGDOOclKh3z6si76qfpto",
+ "privateKey":"i1MxC-K9kvs8St3o2prBNvwz5Uzd7sZNm_MW8wXTYBE"
+};
+
+webpush.setVapidDetails(
+    'mailto:example@yourdomain.org',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
@@ -19,6 +31,40 @@ app.post('/subscribe', function(req, res) {
 
  res.send(req.body);
 });
+
+app.route('/api/newsletter').post(sendNewsletter);
+
+function sendNewsletter(req, res) {
+
+// const allSubscriptions = ... get subscriptions from database
+
+//console.log('Total subscriptions', allSubscriptions.length);
+
+ const notificationPayload = {
+  "notification": {
+   "title": "Angular News",
+   "body": "Newsletter Available!",
+   "icon": "assets/main-page-logo-small-hat.png",
+   "vibrate": [100, 50, 100],
+   "data": {
+    "dateOfArrival": Date.now(),
+    "primaryKey": 1
+   },
+   "actions": [{
+    "action": "explore",
+    "title": "Go to the site"
+   }]
+  }
+ };
+
+ Promise.all(allSubscriptions.map(sub => webpush.sendNotification(
+     sub, JSON.stringify(notificationPayload) )))
+     .then(() => res.status(200).json({message: 'Newsletter sent successfully.'}))
+     .catch(err => {
+      console.error("Error sending notification, reason: ", err);
+      res.sendStatus(500);
+     });
+}
 
 const port = process.env.PORT || 80;
 
