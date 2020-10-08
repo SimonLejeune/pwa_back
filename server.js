225026526ce96e0a3c6cd74daf9fb53207dd7368
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-08 16:28:32 +0200

    make it better

--- server.js
+++ server.js
@@ -28,9 +28,10 @@ app.use('/greet', greet);
 
 app.post('/subscribe', (req, res) => {
  const subscription = req.body;
- res.status(201).json({});
  const payload = JSON.stringify({ title: 'test' });
+
  console.log(subscription);
+
  webpush.sendNotification(subscription, payload).catch(error => {
   console.error(error.stack);
  });
