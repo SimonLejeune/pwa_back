d4d8a487654bb19a7d2d1cc8a7a1efa2233c1091
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-08 17:20:05 +0200

    make it better

--- server.js
+++ server.js
@@ -1,53 +1,60 @@
 const express = require('express');
-const cors = require('cors')
 const app = express();
+
 const webpush = require('web-push');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var cors = require('cors');
+
+app.use(cors({
+    origin: "https://ngseo.netlify.app"
+}));
 
 const vapidKeys = {
- "publicKey":"BCNVbiY2W4wbKEIEoA7ChkmtKWUB5M4NQ-dN4M1acfFJSPEB2gJpNYbb0AnXT2--pcrGDOOclKh3z6si76qfpto",
- "privateKey":"i1MxC-K9kvs8St3o2prBNvwz5Uzd7sZNm_MW8wXTYBE"
+    "publicKey":"BCNVbiY2W4wbKEIEoA7ChkmtKWUB5M4NQ-dN4M1acfFJSPEB2gJpNYbb0AnXT2--pcrGDOOclKh3z6si76qfpto",
+    "privateKey":"i1MxC-K9kvs8St3o2prBNvwz5Uzd7sZNm_MW8wXTYBE"
 };
 
 webpush.setVapidDetails(
     'mailto:example@yourdomain.org',
     vapidKeys.publicKey,
     vapidKeys.privateKey
-);
+    );
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(cors({
- origin: "https://ngseo.netlify.app"
-}));
-/*app.use(cors());
-app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "https://ngseo.netlify.app"); // update to match the domain you will make the request from
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
-});*/
 
-const greet = express.Router();
-greet.get('/', (req, res) => {
- console.log(req.baseUrl);
- res.send('Hello World');
-})
-app.use('/greet', greet);
-
-app.post('/subscribe', (req, res) => {
- const subscription = req.body;
- const payload = JSON.stringify({ title: 'test' });
-
- console.log(subscription);
-
- webpush.sendNotification(subscription, payload).catch(error => {
-  console.error(error.stack);
- });
+const subscribe = express.Router();
+subscribe.post('/', (req, res) => {
+    const subscription = req.body;
+    const payload = JSON.stringify({
+        "notification": {
+        "title": "Angular News",
+        "body": "Newsletter Available!",
+        "icon": "assets/main-page-logo-small-hat.png",
+        "vibrate": [100, 50, 100],
+        "data": {
+        "dateOfArrival": Date.now(),
+        "primaryKey": 1
+        },
+        "actions": [{
+        "action": "explore",
+        "title": "Go to the site"
+        }]
+    }
 });
+    console.log(subscription);
+    webpush.sendNotification(subscription, payload).then(result => {
+        console.log(result);
+        res.send(result);
+    }).catch(error => {
+      res.send(error.stack);
+    });
+    res.send(payload);
+})
+
+app.use('/subscribe', subscribe);
 
 const port = process.env.PORT || 80;
 
-app.listen(port, err => {
- if(err) throw err;
- console.log("%c Server running", "color: green");
-});
+app.listen(port, () => console.log('server started'));
