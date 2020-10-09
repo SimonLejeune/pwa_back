b35b025abaff0177af976b8188e88215b6246e90
Simon Lejeune <simon.lejeune@epitech.eu>
2020-10-09 14:06:25 +0200

    push dotenv

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var cors = require('cors');
 
+/*const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000', 'https://ngseo.netlify.app'];
+const origin = req.headers.origin;
+if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+}*/
+
 app.use(cors({
     origin: ["https://ngseo.netlify.app", "http://localhost:4200"]
 }));
